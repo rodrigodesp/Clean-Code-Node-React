eb7ec68149c64bb3b42c3d58cde7c09d2fd33945
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-05-01 23:15:11 -0300

    feat: ensure RemoveAuthentication returns AccountModel on Sucess

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,26 +1,25 @@
-import { UnexpectedError } from './../../../domain/errors/unexpected-error';
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
-import { HttpStatusCode } from '@/data/protocols/http/http-response';
-import { AuthenticationParams } from '@/domain/usecases/authentication';
 import { HttpPostClient } from '@/data/protocols/http/http-post-client';
-export class RemoteAuthentication {
+import { HttpStatusCode } from '@/data/protocols/http/http-response';
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
+import { UnexpectedError } from '@/domain/errors/unexpected-error';
+import { AccountModel } from '@/domain/models/account-model';
+import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication';
+
+export class RemoteAuthentication implements Authentication{
     constructor (
         private readonly url: string,        
-        private readonly httPostClient: HttpPostClient
+        private readonly httPostClient: HttpPostClient<AuthenticationParams, AccountModel>
     ) {}
 
-    async auth (params: AuthenticationParams): Promise<void> {
+    async auth (params: AuthenticationParams): Promise<AccountModel> {
         const httpResponse = await this.httPostClient.post({
             url: this.url,
             body: params
         })
         switch (httpResponse.statusCode) {
-            case HttpStatusCode.ok: break
-            case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
-            case HttpStatusCode.badRequest: throw new UnexpectedError()
-            case HttpStatusCode.notFound: throw new UnexpectedError()            
-            case HttpStatusCode.serverError: throw new UnexpectedError()
-            default: return Promise.resolve()
+            case HttpStatusCode.ok: return httpResponse.body
+            case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()            
+            default: throw new UnexpectedError()
         }        
     }
 }
