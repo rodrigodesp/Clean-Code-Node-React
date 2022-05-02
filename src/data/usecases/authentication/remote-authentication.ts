d49a1eaac0ba9d3e10193c599f1edbbdd0f4b211
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-05-01 21:43:17 -0300

    feat: ensure RemoveAuthentication throws UnexptectedError on default

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,3 +1,4 @@
+import { UnexpectedError } from './../../../domain/errors/unexpected-error';
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
 import { AuthenticationParams } from '@/domain/usecases/authentication';
@@ -14,7 +15,11 @@ export class RemoteAuthentication {
             body: params
         })
         switch (httpResponse.statusCode) {
+            case HttpStatusCode.ok: break
             case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+            case HttpStatusCode.badRequest: throw new UnexpectedError()
+            case HttpStatusCode.notFound: throw new UnexpectedError()            
+            case HttpStatusCode.serverError: throw new UnexpectedError()
             default: return Promise.resolve()
         }        
     }
