31d1d416f126e5dd5d494299dd4a7f551c7902c1
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-05-02 22:44:35 -0300

    refactor: move create AxiosHttpClient to create makeSut

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -4,10 +4,15 @@ import { AxiosHttpClient } from "./axios-http-client";
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
+
+const makeSut = (): AxiosHttpClient => {
+    return new AxiosHttpClient()
+}
+
 describe('AxiosHttpClient', () => {
     test('Should call axios with correct URL', async () => {
         const url = randUrl()
-        const sut = new AxiosHttpClient()
+        const sut = makeSut()
         await sut.post({url: url})
         expect(mockedAxios).toHaveBeenCalledWith(url)
     })
