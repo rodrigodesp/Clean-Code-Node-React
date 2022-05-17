c2da0a745a6fafff3513ca7222208ec0fbdea5f6
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-05-16 23:05:05 -0300

    feat: call Authentication only once

--- src/presentation/test/mock-authentication.tsx
+++ src/presentation/test/mock-authentication.tsx
@@ -5,8 +5,10 @@ import { Authentication, AuthenticationParams } from '@/domain/usecases/authenti
 export class AuthenticationSpy implements Authentication {
   account = mockAccountModel()
   params: AuthenticationParams
+  callsCount = 0
   async auth (params: AuthenticationParams): Promise<AccountModel> {
     this.params = params
+    this.callsCount++
     return await Promise.resolve(this.account)
   }
 }
