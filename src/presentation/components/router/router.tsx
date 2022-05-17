7eba745387f5495b51bc516c174b388c1b0184d7
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-05-16 21:45:26 -0300

    feat: Ensure login calls authentication-remote with correct values

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,31 +1,36 @@
+import { AccountModel } from '@/domain/models/account-model'
+import { mockAccountModel } from '@/domain/test/mock-account'
+import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
 import { Login } from '@/presentation/pages'
 import { Validation } from '@/presentation/protocols/validation'
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-type SutTypes = {
-  validationStub: ValidationStub
-}
-
 class ValidationStub implements Validation {
   errorMessage: string
-  fieldName: string
-  fieldValue: string
 
   validate (fieldName: string, fieldValue: string): string {
-    this.fieldName = fieldName
-    this.fieldValue = fieldValue
     return this.errorMessage
   }
 }
 
+class AuthenticationStub implements Authentication {
+  account = mockAccountModel()
+  params: AuthenticationParams
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
+    this.params = params
+    return await Promise.resolve(this.account)
+  }
+}
+
 const validationStub = new ValidationStub()
+const authenticationStub = new AuthenticationStub()
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login validation={validationStub} />} />
+          <Route path="/" element={<Login validation={validationStub} authentication={authenticationStub}/>} />
         </Routes>
     </BrowserRouter>
   )
