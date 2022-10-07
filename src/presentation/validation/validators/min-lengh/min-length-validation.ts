c7eca6d3128a7bbe108e22ce20ebb3d6dc72d1e1
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-10-07 11:47:33 -0300

    feat: ensure MinLengthValidation returns error if value is invalid

---
+++ src/presentation/validation/validators/min-lengh/min-length-validation.ts
@@ -0,0 +1,9 @@
+import { FieldValidation } from '@/presentation/validation/protocols/field-validation'
+import { InvalidFieldError } from '@/presentation/validation/errors/'
+
+export class MinLengthValidation implements FieldValidation {
+  constructor (readonly field: string, private readonly minLength: number) {}
+  validate (value: string): Error {
+    return value.length < 3 ? new InvalidFieldError() : null
+  }
+}
