ecac027c888b534b09189c7bda960ca25239211d
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-10-07 11:53:24 -0300

    test: ensure MinLengthValidation returns falsy if value is valid

--- src/presentation/validation/validators/min-lengh/min-length-validation.spec.ts
+++ src/presentation/validation/validators/min-lengh/min-length-validation.spec.ts
@@ -4,8 +4,14 @@ import faker from 'faker'
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
-    const sut = new MinLengthValidation(faker.random.word(), 5)
+    const sut = new MinLengthValidation(faker.random.word(), 3)
     const error = sut.validate('aa')
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if value is valid', () => {
+    const sut = new MinLengthValidation(faker.random.word(), 3)
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
