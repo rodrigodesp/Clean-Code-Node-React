c6d70ddeb0b4903c584a7e16e9eb43a7313c1725
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-10-08 16:03:14 -0300

    feat: ensure validationComposite falsy if there is no error

--- src/presentation/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/presentation/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,15 +1,16 @@
 import { FieldValidationSpy } from '../test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
+import faker from 'faker'
 
 type SutTypes = {
   sut: ValidationComposite
   fieldsValidationSpy: FieldValidationSpy[]
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldsValidationSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field')
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
   ]
   const sut = new ValidationComposite(fieldsValidationSpy)
   return {
@@ -18,13 +19,23 @@ const makeSut = (): SutTypes => {
   }
 }
 
+const fieldName = faker.database.column()
+
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const { sut, fieldsValidationSpy } = makeSut()
-    fieldsValidationSpy[0].error = new Error('first_error_message')
-    fieldsValidationSpy[1].error = new Error('second_error_message')
+    const errorMessage = faker.random.words()
+    const { sut, fieldsValidationSpy } = makeSut(fieldName)
+    fieldsValidationSpy[0].error = new Error(errorMessage)
+    fieldsValidationSpy[1].error = new Error(faker.random.word())
 
-    const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('first_error_message')
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBe(errorMessage)
+  })
+
+  test('Should return falsy if there is no error', () => {
+    const { sut } = makeSut(fieldName)
+
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
