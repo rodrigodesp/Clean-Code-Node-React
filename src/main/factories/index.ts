49b0b20b00bde96821bf4eeac4dec429f91c7963
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-10-09 10:00:27 -0300

    refactor: Create factories to Login page authentication

---
+++ src/main/factories/index.ts
@@ -0,0 +1,4 @@
+export * from './http/api-url-factory'
+export * from './http/axios-http-client-factory'
+export * from './pages/login/login-factory'
+export * from './usercases/authentication/remote-authentication-factory'
--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -2,11 +2,15 @@ import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-const Router: React.FC = () => {
+type Props = {
+  children?: React.ReactNode
+}
+
+const Router: React.FC<Props> = ({ children }) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={makeLogin('')} />
+        <Route path="/login" element={makeLogin(children)} />
       </Routes>
     </BrowserRouter>
   )
