884e7cbe7207f1ce103992bf3e9c00aa8f23fb7e
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-10-10 10:50:56 -0300

    chore: release 1.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,79 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
-const { DefinePlugin } = require("webpack");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+const { DefinePlugin } = require('webpack')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main/index.tsx",
+  mode: 'development',
+  entry: './src/main/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", "scss"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', 'scss'],
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+      '@': path.resolve(__dirname, 'src')
+    }
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader']
       },
       {
         test: /\.html$/,
         use: {
-          loader: "html-loader",
-        },
+          loader: 'html-loader'
+        }
       },
       {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        loader: "ts-loader",
+        loader: 'ts-loader'
       },
       {
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader'
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
-              modules: true,
-            },
+              modules: true
+            }
           },
           {
-            loader: "sass-loader",
-          },
-        ],
+            loader: 'sass-loader'
+          }
+        ]
       },
       {
         test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        use: ["file-loader"],
-      },
-    ],
+        use: ['file-loader']
+      }
+    ]
   },
   devServer: {
     port: 9000,
     compress: true,
     hot: true,
-    static: path.join(__dirname, "dist"),
+    static: path.join(__dirname, 'dist'),
     historyApiFallback: true,
-    open: true,
+    open: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
+      template: './public/index.html',
+      filename: 'index.html'
     }),
     new Dotenv(),
     new DefinePlugin({
-      "process.env.API_URL": "https://fordevs.herokuapp.com/api",
-    }),
-  ],
-};
+      'process.env.API_URL': 'https://fordevs.herokuapp.com/api'
+    })
+  ]
+}
