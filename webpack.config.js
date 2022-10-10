4ee1d98c45b0da6d7911ee3823f5faf333b61480
Rodrigo Pereira <rodrigodesp@gmail.com>
2022-10-10 10:48:54 -0300

    feat: Add env var to API URL

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,79 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const Dotenv = require('dotenv-webpack')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const { DefinePlugin } = require("webpack");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/main/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
+  mode: "development",
+  entry: "./src/main/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", "scss"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx', 'scss'],
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.html$/,
-                use: {
-                    loader: 'html-loader'
-                }
-            },
-            {
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,
-                loader: 'ts-loader'
-        }, {
-            test: /\.scss$/,
-            use: [
-            {
-                loader: 'style-loader'
-            },
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: true
-                }
-            },
-            {
-                loader: 'sass-loader'
-            }
-            ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
         },
-        {
-            test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-            use: ['file-loader']
-        }]
-    },
-    devServer: {
-        port: 9000,
-        compress: true,
-        hot: true,
-        static: path.join(__dirname, 'dist'),
-        historyApiFallback: true,
-        open: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html'
-        }),
-        new Dotenv()
-    ]
-}
+      },
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  devServer: {
+    port: 9000,
+    compress: true,
+    hot: true,
+    static: path.join(__dirname, "dist"),
+    historyApiFallback: true,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new Dotenv(),
+    new DefinePlugin({
+      "process.env.API_URL": "https://fordevs.herokuapp.com/api",
+    }),
+  ],
+};
